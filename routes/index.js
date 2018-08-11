4b6e7b7b1adc133f433de4b349d4c9bfac150daf
Riazahmedsg <riazahmedsg@iconixvav.com>
2018-08-11 10:55:44 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('content/index', {
+    title: 'Home Page'
+  });
+});
+
+/* GET about page. */
+router.get('/about', function (req, res, next) {
+  res.render('content/about', {
+    title: 'About Page'
+  });
+});
+
+/* GET contact page. */
+router.get('/contact', function (req, res, next) {
+  res.render('content/contact', {
+    title: 'Contact Page'
+  });
 });
 
 module.exports = router;
